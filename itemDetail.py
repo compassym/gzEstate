1afe33e9f052e78f932d0bfa85bdff6591e77fc8
compass <xjtu.ym@gmail.com>
2016-11-25 23:58:26 +0800

    实现了房屋部分细节信息的抓取

--- itemDetail.py
+++ itemDetail.py
@@ -15,19 +15,13 @@ class ItemDetail:
         self.bs_obj = None
         self.lat = None
         self.lnt = None
-        self.job_name = None
-        self.enterprise = None
-        self.pay_upper = None
-        self.pay_lower = None
-        self.pay_unit = None
-        self.experience_lower = None
-        self.experience_upper = None
-        self.experience_unit = None
-        self.descriptions = None
-        self.requirements = None
-        self.company_size = None
-        self.company_field = None
-        self.company_financing = None
+        self.price_shoufu = None
+        self.price_tax = None
+        self.nianxian = None
+        self.fangben = None
+        self.guapai_shijian = None
+        self.last_transaction = None
+        self.school = None
 
         self._get_detail(page_link)
 
@@ -42,14 +36,10 @@ class ItemDetail:
             if response.status_code == 200:
                 self.bs_obj = BeautifulSoup(response.content, "lxml")
                 self.lat, self.lnt = self.get_position(self.bs_obj)
-                self.job_name, self.enterprise = self.get_job_name(self.bs_obj)
-                self.pay_lower, self.pay_upper, self.pay_unit = self.get_pay(self.bs_obj)
-                self.experience_lower, self.experience_upper, self.experience_unit \
-                    = self.get_experience(self.bs_obj)
-                self.descriptions, self.requirements \
-                    = self.get_description_requirement(self.bs_obj)
-                self.company_size, self.company_field, self.company_financing \
-                    = self.get_company_info(self.bs_obj)
+                self.get_price_shoufu()
+                self.get_nianxian()
+                self.get_school()
+                self.get_transaction_info()
         except RemoteDisconnected as e:
             logging.error(e)
 
@@ -69,121 +59,73 @@ class ItemDetail:
         except AttributeError:
             return None, None
 
-    def get_job_name(self, bs_obj):
+    def get_price_shoufu(self):
         """
-        从BeautifulSoup对象提取职位名称，对应的公司
-        :param bs_obj: 从网页构造的BeautifulSoup对象
-        :return: tuple对象, ("职位名称"，"公司")
+        从BeautifulSoup对象提取首付及税费信息
+        :return: None
         """
         try:
-            job_name = bs_obj.find("div", {"class": "cont"})\
-                             .find("strong", {"class": "padding-r10"})\
-                             .get_text()
-            enterprise = bs_obj.find("div", {"class": "c_name"})\
-                               .find("a")\
-                               .get_text()
-            logging.debug("Job: (%s, %s)" % (job_name, enterprise))
-            return job_name.strip(), enterprise.strip()
+            tax_node = self.bs_obj.find("div", {"class": "tax"})
+            price_shoufu_txt = tax_node.find("span").get_text()
+            self.price_shoufu = re.match(r"[^0-9]*((?:\d+)(?:\.\d+|)).*", price_shoufu_txt).groups()[0]
+            # TODO 这里需要修正
+            # 爬虫得到的是一个死数，实际上应该是一个js计算结果
+            self.price_tax = tax_node.find("span", {"id": "PanelTax"}).get_text()
+            logging.debug("首付: %s, 税费: %s" % (self.price_shoufu, self.price_tax))
         except AttributeError:
-            return None, None
+            pass
 
-    def get_pay(self, bs_obj):
+    def get_nianxian(self):
         """
-        从BeautifulSoup对象提取薪水
-        :param bs_obj: 从网页构造的BeautifulSoup对象
-        :return: tuple对象，("下限"，"上限"，"单位")
+        从BeautifulSoup对象提取房屋年限信息
+        :return: None
         """
         try:
-            pay = bs_obj.find("span", {"class": "padding-r10 pay"}) \
-                .get_text()
-            lower, upper, unit = self.get_range(pay)
-            logging.debug("薪资范围: [%s-%s]%s" %
-                          (lower, upper, unit))
-            return lower.strip(), upper.strip(), unit.strip()
+            area_node = self.bs_obj.find("div", {"class": "area"})
+            self.nianxian = area_node.find("div", {"class": "subInfo"}).get_text()
+            logging.debug("年限: %s" % self.nianxian)
         except AttributeError:
-            return None, None, None
+            pass
 
-    def get_experience(self, bs_obj):
+    def get_school(self):
         """
-        从BeautifulSoup对象提取公司要求的经验值
-        :param bs_obj: 从网页构造的BeautifulSoup对象
-        :return: tuple对象，("下限"，"上限"，"单位")
+        从BeautifulSoup对象提取房屋学位
+        :return: None
         """
         try:
-            pay = bs_obj.find("span", {"class": "padding-r10 experience"}) \
-                .get_text()
-            lower, upper, unit = self.get_range(pay)
-            logging.debug("经验要求: [%s-%s]%s" %
-                          (lower, upper, unit))
-            return lower.strip(), upper.strip(), unit.strip()
+            node = self.bs_obj.find("div", {"id": "matchSchool"})
+            self.school = node.find("span", {"class": "fortitle"}).find("a").get_text()
+            logging.debug("对口学校: %s" % self.school)
         except AttributeError:
-            return None, None, None
+            pass
 
-    def get_range(self, txt):
+    def get_transaction_info(self):
         """
-        从形式"[x-yUnit]"的文本中提取数值范围
-        :param txt: 以文本形式提供的数值范围
-        :return: tuple，（x, y, Unit)
+        从BeautifulSoup对象提取房屋交易信息
+        :return: None
         """
         try:
-            pattern = "\[(\d+\s*)-(\d+\s*)(.*)\]"
-            return re.match(pattern, txt).groups()
+            info_node = self.bs_obj.find("div", {"class": "introContent"})\
+                                   .find("div", {"class": "transaction"})\
+                                   .findAll("li")
+            self.guapai_shijian = self.remove_label(info_node[0])
+            self.last_transaction = self.remove_label(info_node[2])
+            self.fangben = self.remove_label(info_node[4])
+            logging.debug("挂牌时间: %s, 上次交易: %s, 房本年限: %s"
+                          % (self.guapai_shijian, self.last_transaction, self.fangben))
+
         except AttributeError:
-            return None, None, None
+            pass
 
-    def get_description_requirement(self, bs_obj):
-        """
-        从BeautifulSoup对象提取职位描述信息
-        :param bs_obj: 从网页构造的BeautifulSoup对象
-        :return: tuple of list ([description1, description2, ...],
-                                [requirement1, requirement2, ...])
-        """
-        try:
-            txt = bs_obj.find("div", {"class": "jobdetail nooverflow"})\
-                        .get_text().strip()
-            items = re.sub(r"[:：;；。]", "\n", txt)
-            items = [item.strip() for item in items.split("\n") if item.strip() != ""]
-            descriptions = []
-            requirements = []
-            is_description = True
-            for item in items[1:]:
-                if not re.match("^\d+.*$", item):
-                    is_description = False
-                    continue
-                if is_description:
-                    descriptions.append(item)
-                else:
-                    requirements.append(item)
-            logging.debug("职位描述:")
-            for item in descriptions:
-                logging.debug(item)
-            logging.debug("职责需求:")
-            for item in requirements:
-                logging.debug(item)
-            return descriptions, requirements
-        except AttributeError:
-            return None, None
-
-    def get_company_info(self, bs_obj):
-        """
-        获取提供职位的公司相关信息，公司规模，业务领域，融资信息等
-        :param bs_obj: 从网页构造的BeautifulSoup对象
-        :return: tuple (公司规模, 业务领域, 融资信息)
-        """
-        try:
-            info_obj = bs_obj.find("div", {"class": "plate company_information"})
-            ci_obj = info_obj.findAll("dl", {"class": "ci_body"})[1]
-            info_item_obj = ci_obj.findAll("dd")
-            info_items = [item.get_text() for item in info_item_obj]
-            logging.debug("公司信息: %s" % info_items)
-            return info_items[0].strip(), info_items[1].strip(), info_items[2].strip()
-        except (AttributeError, IndexError):
-            return None, None, None
+    def remove_label(self, bs_obj):
+        txt = bs_obj.get_text()
+        label = bs_obj.find("span").get_text()
+        return txt.replace(label, "")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    test_links = ["/j/467362", "/j/704560", "/j/730233"]
+    test_links = ["/ershoufang/GZ0002238316.html", "/ershoufang/GZ0002197589.html"]
     for link in test_links:
-        job_detail = ItemDetail(link)
+        house_detail = ItemDetail(link)
