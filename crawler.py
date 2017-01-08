82bc65d1f6eac5df7f35ef05cb87d2099c1612d4
ryverl <xjtu.ym@gmail.com>
2017-01-08 13:01:36 +0800

    为爬虫增加集中处理/独立处理的选项

--- crawler.py
+++ crawler.py
@@ -31,30 +31,50 @@ def consumer_of_thread(in_q, sentinel):
         detail_crawler.write2db(detail)
 
 
-def crawl(town, city):
+def _crawl(db_file, first_page):
     sentinel = object()
     buffer = Queue()
-    db_file = detail_crawler.init_db(town=town, city=city)
-    if db_file:
+    list_crawler_thread = Thread(target=list_crawler.get_items,
+                                 kwargs={"sentinel": sentinel,
+                                         "first_page": first_page,
+                                         "out_q": buffer,
+                                         "callback": None,
+                                         })
+    detail_crawler_threads = [Thread(target=consumer_of_thread,
+                                     args=(buffer, sentinel))
+                              for _ in range(config.cnt_of_worker) ]
+    list_crawler_thread.start()
+    for t in detail_crawler_threads:
+        t.start()
+
+    logging.debug("爬虫工作中...")
+    list_crawler_thread.join()
+    logging.debug("房源列表爬取结束")
+    for t in detail_crawler_threads:
+        t.join()
+
+def _crawl_data2_discrete_db(towns, city):
+    db_files = []
+    for town in towns:
+        db_file = detail_crawler.init_db(town=town, city=city)
         first_page = tools.first_page(town)
-        list_crawler_thread = Thread(target=list_crawler.get_items,
-                                     kwargs={"sentinel": sentinel,
-                                             "first_page": first_page,
-                                             "out_q": buffer,
-                                             "callback": None,
-                                             })
-        detail_crawler_threads = [Thread(target=consumer_of_thread,
-                                         args=(buffer, sentinel))
-                                    for _ in range(config.cnt_of_worker) ]
-        list_crawler_thread.start()
-        for t in detail_crawler_threads:
-            t.start()
+        if db_file:
+            _crawl(db_file, first_page)
+            db_files.append(db_file)
+    return db_files
 
-        logging.debug("爬虫工作中...")
-        list_crawler_thread.join()
-        logging.debug("房源列表爬取结束")
-        for t in detail_crawler_threads:
-            t.join()
+def _crawl_data2_lump_db(towns, city):
+    db_file = detail_crawler.init_db(town=None, city=city)
+    if db_file:
+        for town in towns:
+            first_page = tools.first_page(town)
+            _crawl(db_file, first_page)
+        return [db_file]
 
-        return db_file
+
+def crawl(towns, city, discrete=True):
+    if discrete:
+        return _crawl_data2_discrete_db(towns, city)
+    else:
+        return _crawl_data2_lump_db(towns, city)
 
